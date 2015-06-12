63bc98f212b0db79ba86006e19415748b2ed1ab5
Tina Dominguez <tina@tinadominguez.com>
2015-06-12 00:17:06 -0400

    Presidents
    A brief view into US History

---
+++ js/main.js
@@ -0,0 +1,53 @@
+var Obama = {
+    firstName: 'Barack',
+   lastName: 'Obama',
+    termLength: 'two terms',
+    party: 'democrat',
+    yearsofPresidency: 'seven',
+};
+
+var Bush = {
+    firstName: 'George W.',
+   lastName: 'Bush',
+    termLength: 'two terms',
+    party: 'republican',
+    yearsofPresidency: 'eight',
+};
+
+var Clinton = {
+    firstName: 'Bill',
+   lastName: 'Clinton',
+    termLength: 'two terms',
+    party: 'democrat',
+    yearsofPresidency: 'eight',
+};
+
+var Bush Sr. = {
+    firstName: 'George H.W',
+   lastName: 'Bush',
+    termLength: 'one terms',
+    party: 'republican',
+    yearsofPresidency: 'four',
+};
+
+var Reagan = {
+    firstName: 'Ronald',
+   lastName: 'Reagan',
+    termLength: 'two terms',
+    party: 'republican',
+    yearsofPresidency: 'eight',
+};
+
+var presidents = [
+    'Obama',
+    'Bush',
+    'Clinton',
+    'Bush Sr.',
+    'Reagan',
+];
+    
+
+function presidents() {
+        console.log('firstName' + 'lastName');
+    }
+    
