e4b7024d3a7298746c33f084d6e7aa8d208bfd02
Tina Dominguez <tina@tinadominguez.com>
2015-06-12 00:46:19 -0400

    Functions
    Functions

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// function declaration
+function howdy(person, message) {
+        console.log(person + ' says' + message);
+    }
+
+function howdy();
+
+//function expression
+
+if (numba > 7) {
+    console.log('What a short little word!');
+} 
+
+else if (numba = 7 {
+    console.log('7, what a perfect choice!');
+}
+else {
+    console.log('I’m sorry, but that’s too many to count.');
+}
+
+//function inception
+function inception (callback, value) {
+    callback(value);
+}
+
