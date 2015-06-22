9f7317c0c4b91fdfbd6d41c83dd7e4afafa916e5
Richard Kalehoff <richardkalehoff@gmail.com>
2015-06-22 18:04:12 -0400

    create missing 'numba' function

--- js/main.js
+++ js/main.js
@@ -6,14 +6,15 @@ function howdy(person, message) {
 howdy('Tina', 'greetings!');
 
 //function expression
-
-if (numba > 7) {
-    console.log('What a short little word!');
-} else if (numba = 7) {
-    console.log('7, what a perfect choice!');
-} else {
-    console.log('I’m sorry, but that’s too many to count.');
-}
+var numba = function() {
+    if (numba > 7) {
+        console.log('What a short little word!');
+    } else if (numba = 7) {
+        console.log('7, what a perfect choice!');
+    } else {
+        console.log('I’m sorry, but that’s too many to count.');
+    }
+};
 
 //function inception
 function inception (callback, value) {
